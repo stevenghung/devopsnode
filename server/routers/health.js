f996bfbe9b4fab59838428a3aabb3fc7be72f339
stevenghung <sghung@ca.ibm.com>
2018-11-08 00:44:27 -0500

    testing4

--- server/routers/health.js
+++ server/routers/health.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   var router = express.Router();
 
   router.get('/', function (req, res, next) {
-    res.json({status: 'UP 4'});
+    res.json({status: 'UP 5'});
   });
 
   app.use("/health", router);
